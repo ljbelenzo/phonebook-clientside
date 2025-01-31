ee9547b25c8fff53bad03da44fb9244961eefec2
Renzo Jay Belenzo <ljbelenzo@gmail.com>
2025-01-31 14:23:27 +0800

    update

--- src/shared/hooks/api-hook.js
+++ src/shared/hooks/api-hook.js
@@ -24,13 +24,13 @@ export const useHttpClient = () => {
         if(!(body instanceof FormData)) headers = {...headers, 'Content-Type':'application/json'};
 
         try{
-        console.log("error:", `${method} ${body} ${serverHost}${url}`);
+        console.log("error:", `${method} ${{...authHeader,...headers}} ${serverHost}${url}`);
 
             const response = await fetch(`${serverHost}${url}`, {
                 method,
                 body,
                 headers: {...authHeader,...headers},
-                // signal: httpAbortControll.signal,
+                signal: httpAbortControll.signal,
             });
 
 
